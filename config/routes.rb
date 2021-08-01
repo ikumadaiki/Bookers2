367351772100424b2b207044bb4e35278f741a0a
ikumadaiki <ikuma.daiki.infratop@gmail.com>
2021-08-01 17:16:42 +0000

    [Add3] Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'about'=>'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :edit , :update , :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :books, only: [:create, :index, :show, :edit , :update , :destroy]
+  resources :users, only: [:show, :index , :edit, :update]
 end
